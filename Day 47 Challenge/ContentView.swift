389e02f5071119371e18fdc831f16d03a4baed2e
Shane Donnelly <shanedonnelly1@hotmail.com>
2020-10-03 15:04:09 +0100

    Initial Commit

---
+++ Day 47 Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Day 47 Challenge
+//
+//  Created by Shane on 03/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
